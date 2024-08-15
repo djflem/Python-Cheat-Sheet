781aa97707c213a7fcdc4df48dea51be24561f5b
DJ <djfleming.metis@gmail.com>
2024-08-15 16:55:55 -0700

    More sheet and Readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Python-Cheat-Sheet
+
+Binary
  
+Each 1 in a binary number represents an ever-greater multiple of 2. In a 4-digit number, that means you have the eights place, the fours place, the twos place, and the ones place. Similar to how in decimal you would have the thousands place, the hundreds place, the tens place, and the ones place.
+
+128 - 64 - 32 - 16 - 8 - 4 - 2 - 1
+
+1 --  0  -- 0  -- 1  -- 0  -- 1  -- 0 --  1
+
+128 + 16 + 4 + 1 = 149
+
+Simple as that! Another example: The largest number you can store in 5 digits of binary is 31.
+
+
+Bitwise '&' and '|' Operators (AND/OR)
+
+0101 = 5
+&
+0111 = 7
+=
+0101 = 5
+
+& operator requires both to be 1's to be True, so the 01(1)1 is ignored.
+
+0101 = 5
+|
+0111 = 7
+=
+0111 = 7
+
+| operator only requires one of them to be 1's to be True, so the 01(1)1 is not ignored.
--- cheat_sheet/cheat_sheet.py
+++ cheat_sheet/cheat_sheet.py
@@ -1,12 +1,15 @@
+# VARIABLES-------------------------------------------------
+
 my_var = 10 + 4 / 2
 my_name = "DJ"
-is_true = True
+is_true = not False
 none_type_variable = None
 sword_name, sword_damage, sword_length = "Slayer", 15, 8
 number_flooring = 7 // 3
 number_exponent = 3 ** 2
 another_number = 16_000_000  # underscore for readability
-# comment
+
+# PRINTING-------------------------------------------------
 
 print(f"my name is {type(my_name)}")
 print(f"my var is {my_var}")
@@ -14,6 +17,9 @@ print(none_type_variable is None)  # default value to be replaced later
 print(round(my_var))  # avg
 print(16e3)  # 16000.0
 print(7.1e-2)  # 0.071
+
+
+# FUNCTIONS-------------------------------------------------
 
 
 def area_of_circle(radius):
@@ -32,8 +38,62 @@ def take_magic_damage(health, resist, amp, spell_power):
     return health - reduced_dmg
 
 
+def combat_evaluation(player_power, enemy_defense):
+    advantage, disadvantage, evenly_matched = False, False, False
+
+    if player_power > enemy_defense:
+        advantage = True
+    elif player_power == enemy_defense:
+        evenly_matched = True
+    else:
+        disadvantage = True
+
+    return advantage, disadvantage, evenly_matched
+
+
+# LOOPS-------------------------------------------------
+
+
+def print_numbers():
+    for i in range(0, 5, 1):  # 3rd number is increment
+        print(i)
+
+
+def sum_of_odd_numbers(end):
+    total = 0
+    for i in range(1, end, 2):
+        total += i
+    return total
+
+
+def regenerate(current_health, max_health, enemy_distance):
+    while enemy_distance > 3 and current_health < max_health:
+        current_health += 1
+        enemy_distance -= 2
+    return current_health
+
+
+def calculate_experience_points(level):
+    total_exp = 0
+    while level > 1:
+        level -= 1
+        total_exp += exp_per_lvl(level)
+
+    return total_exp
+
+
+def exp_per_lvl(level):
+    exp = level * 5
+    return exp
+
+
+# MAIN-------------------------------------------------
+
+
 def main():  # clear entry points and defined vars/functions
-    print("Running Cheat Sheet!")
+    print("Running Cheat Sheet!\n-------------------")
+    print_numbers()
+    print(f"Exp needed for lvl 2: {calculate_experience_points(2)}")
 
 
 main()
